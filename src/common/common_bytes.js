9d01d44f60bb09674ee383a3858a98948e0e3278
Dowland Aiello <mitsukomegumii@gmail.com>
2019-05-25 09:06:30 -0400

    src/common: Created common_bytes.js.

---
+++ src/common/common_bytes.js
@@ -0,0 +1,17 @@
+/**
+ * Concatenate two byte arrays of the same type.
+ *
+ * @param {any} a
+ * @param {any} b
+ * @return {any} Concatenated array.
+ */
+function concatTypedArrays(a, b) {
+  const c = new a.constructor(a.length + b.length); // Initialize
+
+  c.set(a, 0); // Set first half
+  c.set(b, a.length); // Set second half
+
+  return c; // Return concatenated
+}
+
+module.exports = [concatTypedArrays]; // Exports

3d8125f8e3d4747679694958ad3d1f03ed473b1b
Dowland Aiello <mitsukomegumii@gmail.com>
2019-05-25 12:17:52 -0400

    js-summercash: Migrated to recommended prettier.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,3 @@
 module.exports = {
-  env: {
-    browser: true,
-    commonjs: true,
-    es6: true,
-    node: true,
-  },
-  extends: 'google',
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
-  parserOptions: {
-    ecmaVersion: 2018,
-  },
-  rules: {
-    'max-len': ['error', 80],
-  },
+  extends: ['plugin:prettier/recommended'],
 };

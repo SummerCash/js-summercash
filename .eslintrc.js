96aaeae55e28e1cce77fb141019ba0f583f212e0
Dowland Aiello <mitsukomegumii@gmail.com>
2019-05-25 14:13:43 -0400

    js-summercash: Edited .eslintrc.js, pkg.

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,11 +6,11 @@ module.exports = {
     node: true,
   },
   extends: ['plugin:prettier/recommended'],
-  globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
-  },
   parserOptions: {
     ecmaVersion: 2018,
+  },
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error',
   },
 };

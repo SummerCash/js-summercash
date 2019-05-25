1f9f64c3e0c6371319ef28e9e17ddf1419db219e
Dowland Aiello <mitsukomegumii@gmail.com>
2019-05-25 08:53:28 -0400

    src/common, test: Sync...

--- src/common/common_types.js
+++ src/common/common_types.js
@@ -26,6 +26,28 @@ class Hash {
   constructor(hash) {
     this.hash = hash; // Set hash
   }
+
+  /**
+   * Creates a new instance of Hash from a given hex value.
+   * @param {String} s
+   * @return {Hash} Constructed hash.
+   */
+  static fromString(s) {
+    const encoder = new TextEncoder(); // Initialize encoder
+
+    const hashBody = Buffer.from(s.slice(2, s.length)); // Decode hash body
+
+    return this(encoder.encode('0x') + hashBody); // Decode
+  }
+
+  /**
+   * Serializes hash to string.
+   *
+   * @return {String} String value.
+   */
+  string() {
+    return this.hash.toString('hex'); // Return hex value
+  }
 }
 
 module.exports = [Address, Hash]; // Export address class
--- test/transaction.js
+++ test/transaction.js
@@ -1,9 +1,10 @@
 const assert = require('assert'); // Tests
+const { Transaction } = require('../src/types/transaction'); // Tx class
 
 describe('Transaction', () => {
   describe('#bytes()', () => {
     it('should serialize the given transaction to a Uint8Array', () => {
-
+      const transaction = new Transaction(0, )
     });
   });
 });

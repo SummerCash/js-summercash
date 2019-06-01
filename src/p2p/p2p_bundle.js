77fb16b7ba3168e716cd828a0eea5e9f0ca8a251
Dowland Aiello <mitsukomegumii@gmail.com>
2019-06-01 13:43:22 -0400

    p2p: Created p2p_bundle.js.

---
+++ src/p2p/p2p_bundle.js
@@ -0,0 +1,20 @@
+const libp2p = require('libp2p'); // Libp2p
+const WebSocketStar = require('libp2p-websocket-star'); // Websockets
+const KadDHT = require('libp2p-kad-dht'); // KadDHT
+
+/**
+ * @author: Dowland Aiello
+ */
+class Bundle extends libp2p {
+  constructor(peerInfo) {
+    const wstar = new WebSocketStar(); // Init websocket star
+
+    const modules = {
+      transport: [wstar],
+      discovery: [wstar.discovery],
+      DHT: kadDHT,
+    }; // Set modules
+
+    super(modules, peerInfo); // Call super
+  }
+}
